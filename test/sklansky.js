55323333c3cff571c4d0525c6cba414d3f7b25f1
Aliaksei Chapyzhenka <alex.drom@gmail.com>
2016-05-14 00:15:41 -0700

    added Sklansky algorithms

--- test/sklansky.js
+++ test/sklansky.js
@@ -3,27 +3,30 @@
 var lib = require('../lib');
 
 function printf (r) {
-    return '[' + r.res.hi   + ',' + r.res.lo   + '] ='
-        + ' [' + r.left.hi  + ',' + r.left.lo  + '] +'
-        + ' [' + r.right.hi + ',' + r.right.lo + ']\n';
+    return '[' + r.hi   + ',' + r.lo   + ']';
+}
+
+function printa (r) {
+    return printf(r.res) + ' = '
+        + printf(r.left) + ' + ' + printf(r.right) + '\n';
 }
 
 describe('sklansky', function () {
     it('8', function (done) {
         var res = '';
-        lib.sklansky(8, r => res += printf(r) );
+        lib.sklansky(8, r => res += printa(r) );
         console.log(res);
         done();
     });
     it('7', function (done) {
         var res = '';
-        lib.sklansky(7, r => res += printf(r) );
+        lib.sklansky(7, r => res += printa(r) );
         console.log(res);
         done();
     });
     it('9', function (done) {
         var res = '';
-        lib.sklansky(9, r => res += printf(r) );
+        lib.sklansky(9, r => res += printa(r) );
         console.log(res);
         done();
     });
